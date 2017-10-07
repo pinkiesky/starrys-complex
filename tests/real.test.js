7246e5f2991b5eb964dbbaa579013549e7aca03e
rglr <spore_09@mail.ru>
2017-10-07 20:10:23 +0900

    Fixed TaxMode usage

--- tests/real.test.js
+++ tests/real.test.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 const uuid = require("uuid/v4");
 const createAgent = require('../utils/getHttpsAgent');
-const { Client, Tax, thousandth } = require('../Starrys');
+const { Client, Tax, thousandth, TaxMode } = require('../Starrys');
 
 
 const instance = axios.create({ httpsAgent: createAgent() });
@@ -11,7 +11,7 @@ client
     .addLine({Qty: thousandth(1), Price: 100, TaxId: Tax.VAT18, Description: 'Тест 1'})
     .addLine({Qty: thousandth(2), Price: 1, TaxId: Tax.WITHOUT, Description: 'Тест 2'})
     .set('RequestId', uuid())
-    .set('TaxMode', 1)
+    .set('TaxMode', TaxMode.REGULAR)
     .set('ClientId', 'abc')
     .proceed()
     .then(console.log)
